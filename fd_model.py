705f6061a152d1dd586e3e67a7d974904658f5a3
Tanish Baranwal <b.tanish@tugun.ai>
2018-11-03 15:37:07 -0700

    first working version

--- fd_model.py
+++ fd_model.py
@@ -11,77 +11,77 @@ class FdModel(object):
     self.FEATURE_NAMES = self.data_prep.FEATURE_NAMES
 
   def autoencoder_model_fn(self, features, labels, mode, params):
-    
+
     feature_columns = list(self.data_prep.get_feature_columns().values())
-    
+
     input_layer_size = len(feature_columns)
-    
+
     encoder_hidden_units = params.encoder_hidden_units
-    
+
     # decoder units are the reverse of the encoder units, without the middle layer (redundant)
-    decoder_hidden_units = encoder_hidden_units.copy()  
+    decoder_hidden_units = encoder_hidden_units.copy()
     decoder_hidden_units.reverse()
     decoder_hidden_units.pop(0)
-    
+
     output_layer_size = len(self.FEATURE_NAMES)
-    
+
     he_initialiser = tf.contrib.layers.variance_scaling_initializer()
     l2_regulariser = tf.contrib.layers.l2_regularizer(scale=params.l2_reg)
-    
-    
+
+
     print("[{}]->{}-{}->[{}]".format(len(feature_columns)
                      ,encoder_hidden_units
                      ,decoder_hidden_units,
                      output_layer_size))
 
     is_training = (mode == tf.estimator.ModeKeys.TRAIN)
-    
-    # input layer
-    input_layer = tf.feature_column.input_layer(features=features, 
-                          feature_columns=feature_columns)
-    
-    # Adding Gaussian Noise to input layer
-    noisy_input_layer = input_layer + (params.noise_level * tf.random_normal(tf.shape(input_layer)))
-    
-    # Dropout layer
-    dropout_layer = tf.layers.dropout(inputs=noisy_input_layer, 
-                     rate=params.dropout_rate, 
-                     training=is_training)
 
-  #   # Dropout layer without Gaussian Nosing
-  #   dropout_layer = tf.layers.dropout(inputs=input_layer, 
-  #                     rate=params.dropout_rate, 
-  #                     training=is_training)
+    # input layer
+    input_layer = tf.feature_column.input_layer(features=features,
+                          feature_columns=feature_columns)
+
+    # Adding Gaussian Noise to input layer
+#    noisy_input_layer = input_layer + (params.noise_level * tf.random_normal(tf.shape(input_layer)))
+
+    # Dropout layer
+#    dropout_layer = tf.layers.dropout(inputs=noisy_input_layer,
+#                     rate=params.dropout_rate,
+#                     training=is_training)
+
+     # Dropout layer without Gaussian Nosing
+    dropout_layer = tf.layers.dropout(inputs=input_layer,
+                       rate=params.dropout_rate,
+                       training=is_training)
 
     # Encoder layers stack
     encoding_hidden_layers = tf.contrib.layers.stack(inputs= dropout_layer,
                              layer= tf.contrib.layers.fully_connected,
                              stack_args=encoder_hidden_units,
-                             #weights_initializer = he_init,
+                             weights_initializer = he_initialiser,
                              weights_regularizer =l2_regulariser,
                              activation_fn = tf.nn.relu
                             )
     # Decoder layers stack
     decoding_hidden_layers = tf.contrib.layers.stack(inputs=encoding_hidden_layers,
-                             layer=tf.contrib.layers.fully_connected,        
+                             layer=tf.contrib.layers.fully_connected,
                              stack_args=decoder_hidden_units,
-                             #weights_initializer = he_init,
+                             weights_initializer = he_initialiser,
                              weights_regularizer =l2_regulariser,
                              activation_fn = tf.nn.relu
                             )
     # Output (reconstructed) layer
-    output_layer = tf.layers.dense(inputs=decoding_hidden_layers, 
+    output_layer = tf.layers.dense(inputs=decoding_hidden_layers,
                  units=output_layer_size, activation=None)
-    
+
     # Encoding output (i.e., extracted features) reshaped
     encoding_output = tf.squeeze(encoding_hidden_layers)
-    
+
     # Reconstruction output reshaped (for serving function)
     reconstruction_output =  tf.squeeze(tf.nn.sigmoid(output_layer))
-    
+
     # Provide an estimator spec for `ModeKeys.PREDICT`.
     if mode == tf.estimator.ModeKeys.PREDICT:
-      
+
       # Convert predicted_indices back into strings
       predictions = {
         'encoding': encoding_output,
@@ -90,20 +90,20 @@ class FdModel(object):
       export_outputs = {
         'predict': tf.estimator.export.PredictOutput(predictions)
       }
-      
+
       # Provide an estimator spec for `ModeKeys.PREDICT` modes.
       return tf.estimator.EstimatorSpec(mode,
                         predictions=predictions,
                         export_outputs=export_outputs)
-    
+
     # Define loss based on reconstruction and regularization
-    
-    #   reconstruction_loss = tf.losses.mean_squared_error(tf.squeeze(input_layer), reconstruction_output) 
+
+    #   reconstruction_loss = tf.losses.mean_squared_error(tf.squeeze(input_layer), reconstruction_output)
     #   loss = reconstruction_loss + tf.losses.get_regularization_loss()
-    
+
     reconstruction_loss = tf.losses.sigmoid_cross_entropy(multi_class_labels=tf.squeeze(input_layer), logits=tf.squeeze(output_layer))
     loss = reconstruction_loss + tf.losses.get_regularization_loss()
-               
+
     # Create Optimiser
     optimizer = tf.train.AdamOptimizer(params.learning_rate)
 
@@ -116,7 +116,7 @@ class FdModel(object):
       "rmse": tf.metrics.root_mean_squared_error(
         tf.squeeze(input_layer), reconstruction_output)
     }
-                             
+
     # Provide an estimator spec for `ModeKeys.EVAL` and `ModeKeys.TRAIN` modes.
     estimator_spec = tf.estimator.EstimatorSpec(mode=mode,
                           loss=loss,
@@ -125,13 +125,12 @@ class FdModel(object):
     return estimator_spec
 
   def create_estimator(self, run_config, hparams):
-    estimator = tf.estimator.Estimator(model_fn=self.autoencoder_model_fn, 
-                    params=hparams, 
+    estimator = tf.estimator.Estimator(model_fn=self.autoencoder_model_fn,
+                    params=hparams,
                     config=run_config)
-    
+
     print("")
     print("Estimator Type: {}".format(type(estimator)))
     print("")
 
     return estimator
-
--- predict.py
+++ predict.py
@@ -43,8 +43,22 @@ run_config = tf.estimator.RunConfig(
 estimator = fd_model.create_estimator(run_config, hparams)
 
 predictions = estimator.predict(input_fn=input_fn)
-predictions = itertools.islice(predictions, DATA_SIZE)
-predictions = list(map(lambda item: list(item["encoding"]), predictions))
+#predictions = itertools.islice(predictions, DATA_SIZE)
+#predictions = list(map((lambda item: list(item["encoding"])), predictions))
+temp = predictions
+predictions = lambda item: list(item["encoding"])
+print("Lambda \n")
+print (type(predictions))
+print (predictions)
+predictions = map(predictions, temp)
+print("Map \n")
+print (type(predictions))
+print (predictions)
+predictions = list(predictions)
+print("List \n")
+print (type(predictions))
+print (predictions)
+#predictions = list(map((lambda item: list(item["encoding"])), predictions))
 
 print(predictions[:5])
 
@@ -62,5 +76,3 @@ fig = plt.figure(figsize=(15,10))
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(xs=data_reduced.c2/1000000, ys=data_reduced.c3/1000000, zs=data_reduced.c1/1000000, c=data_reduced['class'], marker='o')
 plt.show()
-
-
