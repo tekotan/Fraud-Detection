db3fa864f35cb242a9f4be33a0a654c01ede1f52
Ajay Baranwal <kb.ajay@tugun.ai>
2018-11-25 18:26:21 +0000

    Added tiny run

--- de/run.sh
+++ de/run.sh
@@ -1,2 +1,3 @@
 # To generate selected columns
 python3 training_data_prep.py -i /home/ajay/data/2018/closed_apr -o ./trn_data_out
+#python3 training_data_prep.py -i /home/ajay/data/2018/closed_test_tiny -o ./trn_data_out
